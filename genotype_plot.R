ee8a161d05a9a8b1933c1638119ccb2ddfeac81c
yu052 <50414773+yu052@users.noreply.github.com>
2019-11-06 13:59:32 +0100

    Update genotype_plot.R

--- genotype_plot.R
+++ genotype_plot.R
@@ -1,5 +1,5 @@
 .libPaths("~/~R/library")## need to change to your R package library directory
-return_genotype<-function(pedfile,marker){
+return_genotype<-function(pedfile,marker,png_width,png_height){
   if (!require(ggplot2)) install.packages('ggplot2')## if not installed, install first
   library(ggplot2)
   ped<-read.table(paste(pedfile,'.ped',sep = ""),header = F)
@@ -31,7 +31,7 @@ return_genotype<-function(pedfile,marker){
     plot_list[[i]]<-p
   }
   for (i in 1:length(markers)) {
-    png(filename = paste(markers[i],'_geno.png',sep = ""),width = 1280,height = 720)
+    png(filename = paste(markers[i],'_geno.png',sep = ""),width = png_width,height = png_height)
     print(plot_list[[i]])
     dev.off()
   }
@@ -41,12 +41,14 @@ library(optparse)
 option_list<-list(
   make_option(c('-f','--file'),type = "character",default = NULL,help = "ped file prefix",
               metavar = 'character'),
-  make_option(c('--snp'),type = "character",help = "snp list file",default = NULL)
+  make_option(c('--snp'),type = "character",help = "snp list file",default = NULL),
+  make_option(c('--width'),type = 'integer',help="width of the png pic",default =1280),
+  make_option(c('--height'),type = 'integer',help="height of the png pic",default = 720)
 )
 opt_parser<-OptionParser(option_list = option_list)
 opt<-parse_args(opt_parser)
 
 system("module load plink/1.9-180913")
 system(paste("plink --file ",opt$file," --chr-set 38 --extract ",opt$snp," --recode --out ",opt$file,"_extract",sep=""))
-return_genotype(pedfile = opt$file,marker = opt$snp)
+return_genotype(pedfile = opt$file,marker = opt$snp,png_width=opt$width,png_height=opt$height)
 system(paste("rm ",opt$file,"_extract*",sep=""))
